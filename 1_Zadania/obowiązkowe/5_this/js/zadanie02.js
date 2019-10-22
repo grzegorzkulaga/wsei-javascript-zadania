820801bba15eacbb16afbf409e5783b746c1306c
grzegorzkulaga <poczta@grzegorzkulaga.pl>
2019-10-22 15:14:12 +0200

    Chapter 5 - this, task 2

--- 1_Zadania/obowiązkowe/5_this/js/zadanie02.js
+++ 1_Zadania/obowiązkowe/5_this/js/zadanie02.js
@@ -2,6 +2,14 @@ var calculator = {
     save: function (newA, newB) {
         this.a = newA;
         this.b = newB;
+    },
+
+    sum: function(){
+        return this.a+this.b;
+    },
+
+    multiply: function(){
+        return this.a*this.b;
     }
 
 };
