1ac395306e0e9d8613409c13cd59fa7168f94c36
Grzegorz Kułaga <poczta@grzegorzkulaga.pl>
2019-12-08 10:33:53 +0100

    Chapter 8 - DOM elements

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -3,4 +3,39 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
      */
 
+    // task 0
+    var title = document.querySelector('.title');
+
+    function getDataAnimation(element){
+        var value = element.dataset.animation;
+        return value;
+    }
+    var result = getDataAnimation(title);
+    console.log(result);
+
+    //task 1
+    var home = document.querySelector('#home');
+    var home2 = document.querySelector('home');
+    console.log(home, home2);
+
+    var direction = document.querySelector('li');
+    console.log(direction);
+
+    var block = document.querySelector('.block');
+    console.log(block);
+
+    //task 2
+    const liInNav = document.querySelector('nav').querySelector('li');
+    console.log(liInNav.length);
+
+    const pInDiv = document.querySelectorAll('div p');
+    console.log(pInDiv.length);
+
+    const divsInArticle = document.querySelectorAll('article div');
+    console.log(divsInArticle.length);
+
+    //task 3
+    const articleTag = document.querySelectorAll('article.first');
+    const h1 = articleTag.querySelectorAll('h1').length;
+    console.log(h1);
 });
