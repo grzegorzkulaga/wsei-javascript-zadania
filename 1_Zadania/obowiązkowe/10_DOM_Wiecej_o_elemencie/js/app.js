19487001b2bf9fe7e1b5eaedfbd8bd0682dbcb35
Grzegorz Kułaga <poczta@grzegorzkulaga.pl>
2019-12-21 22:56:44 +0100

    Chapter 10 - More about DOM element, task 1 and 2

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -18,4 +18,36 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    //task 1
+    var chromeLogo = document.querySelector(".chrome");
+    var edgeLogo = document.querySelector(".edge");
+    var firefoxLogo = document.querySelector(".firefox");
+
+    chromeLogo.style.backgroundImage = "url('assets/img/chrome.png')";
+    chromeLogo.style.width = "100px";
+
+    edgeLogo.style.backgroundImage = "url('assets/img/edge.png')";
+    firefoxLogo.style.backgroundImage = "url('assets/img/firefox.png')";
+
+    var chromeLink = document.querySelector(".chrome + a");
+    var edgeLink = document.querySelector(".edge + a");
+    var firefoxLink = document.querySelector(".firefox + a");
+
+    chromeLink.innerHTML = "Chrome";
+    firefoxLink.innerHTML = "Firefox";
+
+    chromeLink.setAttribute("href", "https://www.google.com/intl/pl_pl/chrome/");
+    edgeLink.setAttribute("href", "https://www.microsoft.com/pl-pl/windows/microsoft-edge");
+    firefoxLink.setAttribute("href","https://www.mozilla.org/pl/firefox/new/");
+
+    //task 2
+    var name = document.querySelector(".ex2 #name");
+    var color = document.querySelector(".ex2 #fav_color");
+    var food = document.querySelector(".ex2 #fav_meal");
+
+    name.innerHTML = "Grzegorz Kułaga";
+    color.innerHTML = "Green";
+    food.innerHTML = "Pizza";
+
+    //task 3
 });
