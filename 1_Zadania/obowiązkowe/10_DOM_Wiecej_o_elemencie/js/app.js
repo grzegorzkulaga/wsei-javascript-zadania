233b4d865c000efa596960dd54e748fccfff60db
Grzegorz Kułaga <poczta@grzegorzkulaga.pl>
2019-12-21 21:02:41 +0100

    Chapter 10 - More about DOM element, task 0

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -3,4 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+     //task 0
+    var ex5 = document.querySelector(".ex5 ul").children;
+
+    for(var i=0; i<ex5.length; i++){
+        if(i%2==0){
+            ex5[i].style.backgroundColor = "green";
+        }
+        if(i%3==0){
+            ex5[i].style.textDecoration = "underline";
+        }
+        if(i==4){
+            ex5[i].classList.add("big");
+        }
+    }
+
 });
