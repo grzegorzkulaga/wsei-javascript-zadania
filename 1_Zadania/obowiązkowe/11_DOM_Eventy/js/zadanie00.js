5dfc305261a591572aa6cedc9e1896e4b785602c
Grzegorz Kułaga <poczta@grzegorzkulaga.pl>
2020-01-07 20:20:17 +0100

    Chapter 11 - DOM Evenets - task 0a and 0b

---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie00.js
@@ -0,0 +1,20 @@
+var parentElements = document.getElementsByClassName('parent');
+
+for (var i=0; i<parentElements.length; i++){
+    parentElements[i].addEventListener("mouseenter", function(e){
+        this.children[0].style.display = "block";
+    })
+    parentElements[i].addEventListener("mouseleave", function(e){
+        this.children[0].style.display = "none";
+    })
+}
+
+// task 0b
+for (var i = 0; i < parentElements.length; i++) {
+    parentElements[i].addEventListener("mouseleave", function() {
+        function hideChildrenElement(element){
+            element.firstElementChild.style.display='none';
+        }
+        hideChildrenElement(this);
+    })
+}
