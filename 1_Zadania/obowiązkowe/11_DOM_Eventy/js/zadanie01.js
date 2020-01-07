5727a26ec6878d6d81eb61133cf28820af83caf8
Grzegorz Kułaga <poczta@grzegorzkulaga.pl>
2020-01-07 21:45:39 +0100

    Chapter 11 - DOM Events - task 1

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
@@ -1,5 +1,24 @@
+/*
 var menu = document.querySelector("#menu");
-var paragraf = document.querySelector("p");
+    var paragraf = document.querySelector("p");
+    
+    menu.classList.add("menu");
+    paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+*/
+/* 1
+- Skrypt w head zostanie wywołany w momencie kiedy DOM jeszcze nie jest dostępny.
+- Skrypt na dole kodu strony zostanie pobrany po załadowaniu strony.
+*/
 
-menu.classList.add("menu");
-paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+// 2
+document.addEventListener("DOMContentLoaded",function(){
+    var menu = document.querySelector("#menu");
+    var paragraf = document.querySelector("p");
+    
+    menu.classList.add("menu");
+    paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+});
+// Stworzenie DOMContentLoaded poprawiło sytuacje, ponieważ kod wykonuje się dopiero gdy dokument zostanie pomyślnie załadowany.
+
+// 3
+// Nie ma różnicy - skrypt zostanie wykonany po załadowaniu się kodu HTML.
