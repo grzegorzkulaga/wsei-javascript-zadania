baa4b1d0df5623ee70d1d99909ff9078e810bbd2
Grzegorz Kułaga <poczta@grzegorzkulaga.pl>
2020-01-31 23:42:24 +0100

    Additional chapter - tooltips

--- 1_Zadania/dodatkowe/5_Tooltips/js/app.js
+++ 1_Zadania/dodatkowe/5_Tooltips/js/app.js
@@ -1,3 +1,32 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+document.addEventListener("DOMContentLoaded",function(){
+    var tooltipText = document.querySelectorAll("span.tooltip");
+    var tooltipTextOne = tooltipText[0];
+    var tooltipTextTwo = tooltipText[1];
+    var tooltipTextThree = tooltipText[2];
+    
+    console.log(tooltipTextOne);
+    console.log(tooltipTextTwo);
+    console.log(tooltipTextThree);
+    
+    tooltipTextOne.addEventListener("mouseover", mouseoverUl);
+    tooltipTextOne.addEventListener("mouseout", mouseoutUl);
+    tooltipTextTwo.addEventListener("mouseover", mouseoverUl);
+    tooltipTextTwo.addEventListener("mouseout", mouseoutUl);
+    tooltipTextThree.addEventListener("mouseover", mouseoverUl);
+    tooltipTextThree.addEventListener("mouseout", mouseoutUl);
+    
+    var div;
+    function mouseoverUl(){
+            div = document.createElement("div");
+            div.className = "tooltipText";
+            div.innerHTML = this.dataset.text;
+            this.appendChild(div);
+    }
+
+    function mouseoutUl(){
+        div.remove();
+    }
+});
